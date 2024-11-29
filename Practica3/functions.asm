b62ed4497373dec500888005924d164688df2577
ArielaG-byte <guzmanariela9@gmail.com>
2024-11-28 20:59:15 -0400

    Practica 3

---
+++ Practica3/functions.asm
@@ -0,0 +1,55 @@
+slen:
+    push    ebx
+    mov     ebx, eax
+ 
+nextchar:
+    cmp     byte [eax], 0
+    jz      finished
+    inc     eax
+    jmp     nextchar
+ 
+finished:
+    sub     eax, ebx
+    pop     ebx
+    ret 
+sprint:
+    push    edx
+    push    ecx
+    push    ebx
+    push    eax
+    call    slen
+ 
+    mov     edx, eax
+    pop     eax
+ 
+    mov     ecx, eax
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+ 
+    pop     ebx
+    pop     ecx
+    pop     edx
+    ret 
+;------------------------------------------
+; void sprintLF(String message)
+; String printing with line feed function
+sprintLF:
+    call    sprint
+ 
+    push    eax         
+    mov     eax, 0Ah                       
+    push    eax         
+    call    sprint      
+    pop     eax        
+    pop     eax         
+    ret
+
+
+
+quit:
+    mov     ebx, 0
+    mov     eax, 1
+    int     80h
+    ret
+
